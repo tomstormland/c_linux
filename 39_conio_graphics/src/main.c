298f08a15ad4118d2fdf1ebf453a34b08d8cf45c
tomstormland <tomstormland@gmail.com>
2018-07-06 08:02:37 +0300

    x

---
+++ 39_conio_graphics/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+
+int main(int argc, char *argv[]) {
+
+  return 0;
+}
---
+++ 40_directx_test/readme.md
@@ -0,0 +1,4 @@
+40_directx_test
+===============
+
+http://www.directxtutorial.com/Lesson.aspx?lessonid=9-4-4
