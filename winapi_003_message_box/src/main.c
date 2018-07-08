e9a21cc43cd8ee3cbaf72466ea7a969ef18e2811
ravensim15 <torumees@gmail.com>
2018-07-09 00:03:23 +0300

    x

---
+++ winapi_003_message_box/src/main.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR szCmdLine, int CmdShow) {
+
+  MessageBoxW(NULL, L"LOREM CONTENT", L"LOREM TEXT", MB_OK);
+
+  return 0;
+}
